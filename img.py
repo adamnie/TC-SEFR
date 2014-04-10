06fa8b37963b4f6df40861acafe53c59ff3c4219
anteq <antonigrzanka@gmail.com>
2014-04-10 23:10:22 +0200

    Optimized cropping for D_block and R_block, added img.cutsquare() as generalization of these two. This one allows to cut squares by pixel positions, might be useful.

--- img.py
+++ img.py
@@ -41,55 +41,23 @@ class img(numpy.ndarray):
     # img.blockR(0,0) returns first ([0,0]), img.blockR(0,1) returns second ([0,1])...
     # that goes to img.blockR(127,127) which returns lower right corner (last block)
     # warning: to be consistent, D blocks are 0-indexed!
+
+    def cutsquare(self, x, y, size): # x i y sa w pikselach
+        crop1 = self[y:y+size] # przyciecie pozadanej ilosci wierszy tabeli, czyli przyciecie wertykalne (y)
+        crop2 = numpy.zeros(shape=(size,size))
+        for index in range(size):
+            crop2[index] = (crop1[index])[x:x+size]  #przyciecie pozadanej ilosci elementow w wierszu, czyli przyciecie horyzontalne (x)
+        return crop2.view(img)
+
     def blockR(self, x, y, blocksize):
-        x *= blocksize                                             # NOT SURE ABOUT RIGHT INDEXING!!! WHICH ONE IS HOR & WHICH ONE IS VER!!!
-        y *= blocksize
-        crop1 = self[y:y+blocksize] # przyciecie pozadanej ilosci wierszy tabeli, czyli przyciecie wertykalne (y)
-        crop2 = numpy.zeros(shape=(blocksize,blocksize))
-        for index in range(blocksize):
-            crop2[index] = (crop1[index])[x:x+blocksize]  #przyciecie pozadanej ilosci elementow w wierszu, czyli przyciecie horyzontalne (x)
-        return crop2.view(R_block)
+        sth = self.cutsquare(x*blocksize,y*blocksize,blocksize)
+        return sth.view(R_block)
 
-    """
-    Assuming we have array of R blocks as shown below:
-    +------+------+------+------+------+
-    | R 00 | R 01 | R 02 | R 03 | R 04 |
-    +------+------+------+------+------+
-    | R 10 | R 11 | R 12 | R 13 | R 14 |
-    +------+------+------+------+------+
-    | R 20 | R 21 | R 22 | R 23 | R 24 |
-    +------+------+------+------+------+
-    | R 30 | R 31 | R 32 | R 33 | R 34 |
-    +------+------+------+------+------+
-    | R 40 | R 41 | R 42 | R 43 | R 44 |
-    +------+------+------+------+------+
-    Since D blocks are overlapping, it's desired to identify them ambiguously with the least necessary information given.
-    Our function blockD() takes as an argument index of top-left R block and creates D block consisting of four R blocks. Meaning, for example:
-    +------+------+------+------+------+   +------+------+------+------+------+   +------+------+------+------+------+
-    |             | R 02 | R 03 | R 04 |   | R 00 | R 01 | R 02 | R 03 | R 04 |   | R 00 | R 01 | R 02 | R 03 | R 04 |
-    +    D 00     +------+------+------+   +------+------+------+------+------+   +------+------+------+------+------+
-    |             | R 12 | R 13 | R 14 |   | R 10 |             | R 13 | R 14 |   | R 10 | R 11 | R 12 | R 13 | R 14 |
-    +------+------+------+------+------+   +------+    D 11     +------+------+   +------+------+------+------+------+
-    | R 20 | R 21 | R 22 | R 23 | R 24 |   | R 20 |             | R 23 | R 24 |   | R 20 | R 21 | R 22 | R 23 | R 24 |
-    +------+------+------+------+------+   +------+------+------+------+------+   +------+------+------+------+------+
-    | R 30 | R 31 | R 32 | R 33 | R 34 |   | R 30 | R 31 | R 32 | R 33 | R 34 |   | R 30 | R 31 |             | R 34 
-    +------+------+------+------+------+   +------+------+------+------+------+   +------+------+    D 32     +------+
-    | R 40 | R 41 | R 42 | R 43 | R 44 |   | R 40 | R 41 | R 42 | R 43 | R 44 |   | R 40 | R 41 |             | R 44 |
-    +------+------+------+------+------+   +------+------+------+------+------+   +------+------+------+------+------+
-    and so on.
-    That means that highest index can be (width-2,height-2)
-    """
-
-	# sets D block of size=2*blocksize and number [h,v] as defined above 
-	def blockD(self, x, y, blocksize):
+    # sets D block of size=2*blocksize and number [h,v] as defined above 
+    def blockD(self, x, y, blocksize):
 		if (x <= (self.width()/blocksize - 2) and y <= (self.height()/blocksize)-2):
-			x *= blocksize
-			y *= blocksize
-			crop1 = self[y:y+2*blocksize]
-			crop2 = numpy.zeros(shape=(2*blocksize,2*blocksize))
-			for index in range(2*blocksize):
-				crop2[index] = (crop1[index])[x:x+2*blocksize]
-			return crop2.view(R_block)
+			sth = self.cutsquare(x*blocksize,y*blocksize,2*blocksize)
+			return sth.view(D_block)
 		else:
 			print "Blad indeksowania D_block"		
 			return -1
