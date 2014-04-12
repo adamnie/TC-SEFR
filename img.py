1fc1d8ffbcb24fbed54559f1d2893cf2122a1d15
anteq <antonigrzanka@gmail.com>
2014-04-12 15:02:05 +0200

    Added requested delta feature (without modulo). Tests are promising, however not sure if it works 100% properly. Lets hope it does.

--- img.py
+++ img.py
@@ -1,6 +1,6 @@
 """
-	img.py
-	Defining classes with all needed methods.
+    img.py
+    Defining classes with all needed methods.
 """
 
 
@@ -30,14 +30,15 @@ class img(numpy.ndarray):
     # plots normalized image in grayscale
     def plot(self): #in grayscale
         plt.imshow(self, cmap=plt.cm.gray, norm=plt.Normalize(0,255))
-        plt.show()
+        plt.ion()
+        plt.show(block=True)
 
     # exports image to file
     def export(filename):
         misc.imsave(filename,self)
 
-    # sets R block of size=blocksize and number [h,v] 
-    # e.g. when you have 512x512 image and blocksize=4, you have 128x128 R blocks
+    # sets R block of size=Rsize and number [h,v] 
+    # e.g. when you have 512x512 image and Rsize=4, you have 128x128 R blocks
     # img.blockR(0,0) returns first ([0,0]), img.blockR(0,1) returns second ([0,1])...
     # that goes to img.blockR(127,127) which returns lower right corner (last block)
     # warning: to be consistent, D blocks are 0-indexed!
@@ -49,30 +50,48 @@ class img(numpy.ndarray):
         for index in range(size):
             crop2[index] = (crop1[index])[x:x+size]  #przyciecie pozadanej ilosci elementow w wierszu, czyli przyciecie horyzontalne (x)
         return crop2.view(img)
-
-    def blockR(self, x, y, blocksize):
-        sth = self.cutsquare(x*blocksize,y*blocksize,blocksize)
+   
+    def blockR(self, x, y, Rsize):
+        sth = self.cutsquare(x*Rsize,y*Rsize,Rsize)
         return sth.view(R_block)
+    
+    #poki co bez modulo, ok? 
 
-    # sets D block of size=2*blocksize and number [h,v] as defined above 
-    def blockD(self, x, y, blocksize):
-		if (x <= (self.width()/blocksize - 2) and y <= (self.height()/blocksize)-2):
-			sth = self.cutsquare(x*blocksize,y*blocksize,2*blocksize)
-			return sth.view(D_block)
-		else:
-			print "Blad indeksowania D_block"		
-			return -1
+    def howmanyDinarow(self, Rsize, delta=None):
+        if (delta == None):
+            delta = Rsize
+        return (self.width() - 2*Rsize)/delta + 1
+
+    def howmanyDinacolumn(self, Rsize, delta=None):
+        if (delta == None):
+            delta = Rsize
+        return (self.height() - 2*Rsize)/delta + 1
+
+
+    # size of block D is 2x size of block R
+    # as an argument we pass the same Rsize as to block R! it is multiplied inside the function
+    # delta is the step of selecting next image, default it's equal to Rsize 
+    # x and y are numbers (indexes) of block D, assuming Dsize=2*Rsize and step=delta
+
+    def blockD(self, x, y, Rsize, delta=None):
+        if (x>self.howmanyDinarow(Rsize,delta)-1 or y>self.howmanyDinacolumn(Rsize,delta)-1):
+            print "Blad indeksowania D!!!"
+            return -1
+        if (delta == None):
+            delta = Rsize
+        sth = self.cutsquare(x*delta,y*delta,2*Rsize)
+        return sth.view(D_block)
 
 # class for R_block for special operations
 class R_block(img):
 
-	def plot(self): # overriding inherited plot by one with no interpolation
-		plt.imshow(self, cmap=plt.cm.gray, interpolation='none', norm=plt.Normalize(0,255)) # bez normalizacji normalizuje do max i min z self
-		plt.show()
+    def plot(self): # overriding inherited plot by one with no interpolation
+        plt.imshow(self, cmap=plt.cm.gray, interpolation='none', norm=plt.Normalize(0,255)) # bez normalizacji normalizuje do max i min z self
+        plt.show(block=True)
 
 # class for D_block for special operations
 class D_block(img):
 
-	def plot(self): # overriding inherited plot by one with no interpolation
-		plt.imshow(self, cmap=plt.cm.gray, interpolation='none', norm=plt.Normalize(0,255)) # bez normalizacji normalizuje do max i min z self
-		plt.show()
+    def plot(self): # overriding inherited plot by one with no interpolation
+        plt.imshow(self, cmap=plt.cm.gray, interpolation='none', norm=plt.Normalize(0,255)) # bez normalizacji normalizuje do max i min z self
+        plt.show(block=True)
