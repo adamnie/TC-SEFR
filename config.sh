fbbb721b0a4c032cdad55b62288b4f7d91e95380
anteq <antonigrzanka@gmail.com>
2014-06-24 23:41:38 +0200

    Minor changes.

---
+++ config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python2.7 python-pip python-tk python-imaging-tk python-scipy python-matplotlib
+pip install numpy Pillow scipy matplotlib
+
+
