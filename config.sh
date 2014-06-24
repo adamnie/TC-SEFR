b3ec6ee6146cf8c6ff637b8f06e21a2e03f230e1
anteq <antonigrzanka@gmail.com>
2014-06-25 00:24:57 +0200

    Yup. Changed config.sh.

--- config.sh
+++ config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install python2.7 python-pip python-tk python-imaging-tk python-scipy python-matplotlib
-pip install numpy Pillow scipy matplotlib
-
+sudo apt-get install python2.7 python-pip python-tk python-imaging-tk python-scipy python-matplotlib python-numpy
+pip install Pillow
 
